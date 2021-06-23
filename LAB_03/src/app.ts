43d0050f3855143e5da4ca639fda178ebfa6d9b1
IKOMMM <mikolaj.musialik.media@gmail.com>
2021-06-23 20:55:33 +0200

    LAB_03
    Wczoraj mi prąd padł :( Ale dziś nadrobię!

--- LAB_03/src/app.ts
+++ LAB_03/src/app.ts
@@ -1,27 +1,34 @@
 export class App {
-    opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';
+    opwApiKey = '50d53005c0fd5f556bb4ef15224c4209';  
+
+    
+
     constructor() {
         this.getCityInfo('zakopane')
     }
+
     async getCityInfo(city: string) {
         const weather = await this.getWeather('zakopane');
         this.saveData(weather);
     }
-    async getWeather(city: string): Promise<any> {
-        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`;
-        const weatherResponse = await fetch(openWeatherUrl);
+
+    async getWeather(city: string): Promise<any> {//asynchroniczna metoda, zwraca zawsze objekt Promise 
+        const openWeatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${this.opwApiKey}`; //Zapytanie o pogodę
+        const weatherResponse = await fetch(openWeatherUrl);//pobiera wartość z obietnicy i przypisuje obiekt z jsona z API
         const weatherData = await weatherResponse.json();
         console.log(weatherData);
         return weatherData;
     }
+
     saveData(data: any) {
         localStorage.setItem('weatherData', JSON.stringify(data));
     }
-    getData() {
+
+    getData() {//pobranie danych z localStorage
         const data = localStorage.getItem('weatherData');
         if (data) {
             return JSON.parse(data);
-        } else {
+        } else {//jeżeli użytkownik nic nie dodał
             return {};
         }
     }
